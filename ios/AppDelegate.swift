a05400ec90c531dac1879dae25f9bb27f3b7a71b
Elly <elly@mg.crane.ai>
2018-07-03 14:12:34 +0100

    updated AppDelegate updated appdelegate to support due to network activity adding HUD for ios

--- ios/AppDelegate.swift
+++ ios/AppDelegate.swift
@@ -45,12 +45,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 }
 
 
-        func logout()
-        {
-            let storyboard : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-            let Login = storyboard.instantiateViewController(withIdentifier: "LoginVC") as! LoginVC
-            window?.rootViewController = Login
+        //Mark: General Extension to show loading modal
 
+        extension UIViewController {
+            class func displaySpinner(onView : UIView) -> UIView {
+                let spinnerView = UIView.init(frame: onView.bounds)
+                spinnerView.backgroundColor = UIColor.init(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.5)
+                let ai = UIActivityIndicatorView.init(activityIndicatorStyle: .whiteLarge)
+                ai.startAnimating()
+                ai.center = spinnerView.center
+                
+                DispatchQueue.main.async {
+                    spinnerView.addSubview(ai)
+                    onView.addSubview(spinnerView)
+                }
+                
+                return spinnerView
+            }
+            
+            class func removeSpinner(spinner :UIView) {
+                DispatchQueue.main.async {
+                    spinner.removeFromSuperview()
+                }
+            }
         }
 
         
