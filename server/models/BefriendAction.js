30992abb191af39834d1ebe83968872a3809063f
Elly <elly@mg.crane.ai>
2018-07-03 14:10:51 +0100

    added BefriendAction models

---
+++ server/models/BefriendAction.js
@@ -0,0 +1,24 @@
+// Load required packages
+var mongoose = require('mongoose');
+// Define our BefriendAction schema
+
+var Schema = new mongoose.Schema({
+     _id      :{ type: String }, 
+     createdAt      :{ type: Date }, 
+     updatedAt      :{ type: Date }, 
+     startTime      :{ type: Timestamp }, 
+     result      :{ type: Object }, 
+     participant      :{ type: Object }, 
+     object      :{ type: String }, 
+     location      :{ type: Object }, 
+     instrument      :{ type: String }, 
+     endTime      :{ type: Timestamp }, 
+     agent      :{ type: Object }, 
+     url      :{ type: String }, 
+     name      :{ type: String }, 
+     image      :{ type: String }, 
+     description      :{ type: String }, 
+});
+
+// Export the Mongoose model
+module.exports = mongoose.model('BefriendAction', Schema);
