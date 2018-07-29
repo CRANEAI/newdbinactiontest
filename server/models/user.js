7649bf3a4ac49e125b639301aff169d2cc15df9c
Elly <elly@mg.crane.ai>
2018-07-29 10:09:39 +0200

    updated user updated user model schema

--- server/models/user.js
+++ server/models/user.js
@@ -2,7 +2,11 @@
             const mongoose = require('mongoose');
             
             var Schema = new mongoose.Schema({
-                _id    :  {type: String },username    :  {type: String },password    :  {type: String },email    :  {type: String }
+                _id    :  {type: String } 
+,username    :  {type: String } 
+,password    :  {type: String } 
+,email    :  {type: String } 
+
             })
 
             module.exports = mongoose.model('user', Schema);
