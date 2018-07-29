fd0e76538c3312f89ba3d79bcda32e91cfce43a3
Elly <elly@mg.crane.ai>
2018-07-29 09:36:09 +0200

    added user updated login api route

---
+++ server/api/v1/routes/user.js
@@ -0,0 +1,10 @@
+
+            const router = require("express").Router();
+            
+            /*
+            * login function 
+            */
+            router.post('/login', (req, res) => {
+                res.status(200).send({})
+            })
+            
