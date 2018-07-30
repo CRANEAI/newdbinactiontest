e938e95e84e53aa583382b80c9f17153a14a295d
Ryan Hickman <kingsrborn@gmail.com>
2018-07-30 19:02:28 -0400

    seconds

--- server/api/v1/routes/user.js
+++ server/api/v1/routes/user.js
@@ -1,6 +1,8 @@
 
             const router = require("express").Router();
             
+            console.log("seconds")
+            
             /*
             * login function 
             */
